c93a1add1e919fc469f7a23f627f1b4088d6a270
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-07-31 23:10:48 +0530

    Initial Commit

---
+++ src/main/java/com/springsecurity/embarkx/Spring_Security_EmbarkX/SpringSecurityEmbarkXApplication.java
@@ -0,0 +1,11 @@
+package com.springsecurity.embarkx.Spring_Security_EmbarkX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityEmbarkXApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityEmbarkXApplication.class, args);
+    }
+}
---
+++ src/test/java/com/springsecurity/embarkx/Spring_Security_EmbarkX/SpringSecurityEmbarkXApplicationTests.java
@@ -0,0 +1,11 @@
+package com.springsecurity.embarkx.Spring_Security_EmbarkX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEmbarkXApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}

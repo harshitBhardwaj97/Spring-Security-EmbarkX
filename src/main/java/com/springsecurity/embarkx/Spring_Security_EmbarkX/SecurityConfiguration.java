cb5b5ea1282e09ea7a70c6d1ffba6809da481dfe
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-07-31 23:31:16 +0530

    Security configuration class added

---
+++ src/main/java/com/springsecurity/embarkx/Spring_Security_EmbarkX/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.springsecurity.embarkx.Spring_Security_EmbarkX;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}

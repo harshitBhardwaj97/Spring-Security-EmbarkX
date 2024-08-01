8bad49144f2da27bfa896b53f63b49083643f282
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-08-01 12:17:18 +0530

    UserDetailsService bean added

--- src/main/java/com/springsecurity/embarkx/Spring_Security_EmbarkX/SecurityConfiguration.java
+++ src/main/java/com/springsecurity/embarkx/Spring_Security_EmbarkX/SecurityConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,5 +23,20 @@ public class SecurityConfiguration {
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails userOne = User.withUsername("user")
+                .password("{noop}user")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(userOne, admin);
     }
 }
